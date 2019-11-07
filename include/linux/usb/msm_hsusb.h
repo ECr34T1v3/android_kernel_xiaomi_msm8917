08894d69ee5350b8b9fcc0c5de6b66dc54ad6431
Ankan005 <Ankan005@gmail.com>
2019-11-07 12:05:52 +0530

    minimum 2000

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -123,7 +123,7 @@ enum msm_usb_phy_type {
 #define IDEV_CHG_MAX	2000
 #define IUNIT		100
 #define IDEV_HVDCP_CHG_MAX	20000
-#define IDEV_CHG_MIN 1800
+#define IDEV_CHG_MIN 2000
 
 /**
  * Different states involved in USB charger detection.
